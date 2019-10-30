cd55aed1b1305f79b54a842563f2c086b3ddf70d
lawrence <1023270551@qq.com>
2019-10-30 21:28:20 +0800

    commits

--- src/firstday/LongestPalindromeSolution.java
+++ src/firstday/LongestPalindromeSolution.java
@@ -81,7 +81,7 @@ public class LongestPalindromeSolution {
                     String a = sbTemp.toString();
                     String b = sbTemp.reverse().toString();
                     sbTemp.reverse();
-                    if (a.equals(b) && sbTemp.length() > length ) {
+                    if (a.equals(b) && sbTemp.length() > length) {
                         length = sbTemp.length();
                         resultStr = sbTemp.toString();
                     }
@@ -97,21 +97,89 @@ public class LongestPalindromeSolution {
 
     /**
      * 利用动态规划 求出结果
+     * <p>
+     * 构造二维数组
+     * todo 这个是动态规划的典型例题 一定要反复的学习
      *
      * @param s
      * @return
      */
     public static String longestPalindromeNormal(String s) {
 
+        //记录开始和结束
+        int start = 0;
+        int end = 0;
+        int maxLen = 0;
+        int length = s.length();
+        int[][] p = new int[length][length];
 
-        return null;
+        maxLen = 1;
+        // 初始化数组 将每个位置上i==j都初始化成1 或者将相邻的两个相同的元素初始化成1
+        for (int i = 0; i < length; i++) {
+            p[i][i] = 1;
+            if (i <= length - 2 && s.charAt(i) == s.charAt(i + 1)) {
+                start = i;
+                p[i][i + 1] = 1;
+                maxLen = 2;
+            }
+        }
+
+        // 设定长度从3开始
+        for (int l = 3; l <= length; l++) {
+            for (int i = 0; i + l - 1 < length; i++) {
+                // 结束的未知
+                int j = l + i - 1;
+                if (p[i + 1][j - 1] == 1 && s.charAt(i) == s.charAt(j)) {
+                    p[i][j] = 1;
+                    start = i;
+                    maxLen = l;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLen);
     }
+
+    /**
+     * 中心扩散法 这里没有直接实现
+     * todo 直接搬运过来 需要理解
+     *
+     * @param s
+     * @return
+     */
+    public static String longestPalindromeCentralChange(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
 
     public static void main(String[] args) {
 
-        String str = "aacdefcaa";
+        String str = "cbbd";
         String str1 = "aacfedcaa";
-        System.out.println(longestPalindromeByReverseStr(str));
+        String str3 = "babad";
+        System.out.println(longestPalindromeCentralChange(str3));
 
     }
 
