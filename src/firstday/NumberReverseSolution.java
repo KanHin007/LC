ae63dbcb855b42414084e3d849101df732a3bcc9
Lawrence Han <1023270551@qq.com>
2019-10-31 11:49:58 +0800

    整数反转

---
+++ src/firstday/NumberReverseSolution.java
@@ -0,0 +1,73 @@
+package firstday;
+
+/**
+ * @author Lawrence Han
+ * @date 2019.10.31
+ * 整数反转
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 123
+ * 输出: 321
+ *  示例 2:
+ * <p>
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ * <p>
+ * 输入: 120
+ * 输出: 21
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class NumberReverseSolution {
+
+
+    /**
+     * 反转字符串 不断相除得到数字位置 进行合并
+     *
+     * @param x
+     * @return
+     */
+    public static int reverse(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        StringBuilder resultSb = new StringBuilder();
+        // 判断数字的性质 是否是正数还是负数
+        int positive = 1;
+        long temp = x;
+        if (x < 0) {
+            positive = -1;
+            temp = -1L * x;
+        }
+
+        while (temp > 0) {
+            resultSb.append(temp % 10);
+            temp = temp / 10;
+        }
+        int index = 0;
+        while (index < resultSb.length() && resultSb.charAt(index) == '0') {
+            resultSb.deleteCharAt(index);
+        }
+        long result = Long.valueOf(resultSb.toString());
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return positive * (int) result;
+    }
+
+
+    public static void main(String[] args) {
+        //     System.out.println(reverse(123));
+        //     System.out.println(reverse(120));
+        //     System.out.println(reverse(-120));
+        System.out.println(reverse(901000));
+    }
+
+}
